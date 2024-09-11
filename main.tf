6b414da84250270b6c775630ab390e35a42464a0
mjguru1996 <mailmemjguru@gmail.com>
2024-09-11 07:40:55 +0530

    addedcodeaaaa

--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ provider "aws" {
   access_key = "AKIAUGO4K25OTHVAE7YO"
   secret_key = "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"
 }
+
+resource "aws_instance" "web" {
+    ami = "ami-0522ab6e1ddcc7055"
+    instance_type = "t2.micro"
+    tags = {
+    Name = "webserver"
+  }
+}
