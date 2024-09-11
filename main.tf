c21ba09ffbf83952a300c223fc827ac9624986d9
mjguru1996 <mailmemjguru@gmail.com>
2024-09-11 12:04:57 +0530

    added rt

--- main.tf
+++ main.tf
@@ -15,3 +15,20 @@ resource "aws_internet_gateway" "gw" {
     Name = "gw"
   }
 }
+resource "aws_route_table" "prod-rt" {
+  vpc_id = aws_vpc.nab.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod-rt"
+  }
+}
