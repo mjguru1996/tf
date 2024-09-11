edb86576b578987ba8d4126614d6d36cb636769e
mjguru1996 <mailmemjguru@gmail.com>
2024-09-11 11:53:46 +0530

    added

--- main.tf
+++ main.tf
@@ -3,24 +3,9 @@ provider "aws" {
   access_key = "AKIAUGO4K25OTHVAE7YO"
   secret_key = "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"
 }
-
-resource "aws_instance" "web" {
-    ami = "ami-0522ab6e1ddcc7055"
-    instance_type = "t2.micro"
-    tags = {
-    Name = "webserver"
-  }
-}
 resource "aws_vpc" "nab" {
-    cidr_block = "10.1.0.0/16"
-     tags = {
-    Name = "production"
-  }  
-}
-resource "aws_subnet" "public" {
-vpc_id = aws_vpc.nab.id
-cidr_block = "10.1.0.0/24"
-tags = {
-    Name = "production-subnet"
-  }  
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "nab"
+  }
 }
